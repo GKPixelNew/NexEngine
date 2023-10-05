cbd01f488c2a660698f1ad18d8f07dbcfbbd128a
Akiranya <porridgen@gmail.com>
2023-10-05 13:34:48 +0800

    Update gradle convention plugins

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,9 +9,9 @@ repositories {
 }
 
 dependencies {
+    implementation("cc.mewcraft.repo-conventions", "cc.mewcraft.repo-conventions.gradle.plugin", "1.0.0")
     implementation("cc.mewcraft.java-conventions", "cc.mewcraft.java-conventions.gradle.plugin", "1.0.0")
-    implementation("cc.mewcraft.publishing-conventions", "cc.mewcraft.publishing-conventions.gradle.plugin", "1.0.0")
-    implementation("cc.mewcraft.repository-conventions", "cc.mewcraft.repository-conventions.gradle.plugin", "1.0.0")
     implementation("cc.mewcraft.deploy-conventions", "cc.mewcraft.deploy-conventions.gradle.plugin", "1.0.0")
+    implementation("cc.mewcraft.publishing-conventions", "cc.mewcraft.publishing-conventions.gradle.plugin", "1.0.0")
     implementation("cc.mewcraft.paper-plugins", "cc.mewcraft.paper-plugins.gradle.plugin", "1.0.0")
 }
